b362867516bda04833567b1fbb143be6a07d0201
Rajesh-max178 <rajeshjainrj632@gmail.com>
2026-08-19 00:12:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (23.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    
+    bool checkRecursively(int i, const string &s) {
+        if (i >= s.size() / 2) return true;
+        if (s[i] != s[s.size() - i - 1]) return false;
+        return checkRecursively(i + 1, s);
+    }
+
+public:
+    bool isPalindrome(string s) {
+
+        string cleaned = "";
+        for (char c : s) {
+            if (isalnum(c)) {
+                cleaned += tolower(c);
+            }
+        }
+        
+        // Step 2: Aapke recursive logic ko cleaned string par call karo
+        return checkRecursively(0, cleaned);
+    }
+};
